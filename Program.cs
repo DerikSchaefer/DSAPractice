78231b17097ca33fc5923f733ed4ee55c2851475
Derik <Derik.p.schaefer@gmail.com>
2023-02-03 10:44:18 -0800

    Tidying up

--- Program.cs
+++ Program.cs
@@ -3,13 +3,15 @@
     internal class Program
     {
         static void Main(string[] args)
+
+
         {
             Solution solution = new Solution();
 
 
             #region https://leetcode.com/problems/search-insert-position/ search index position
             int[] nums = { 1, 3, 5, 6 };
-            int target = 2;
+            int target = 7;
             solution.SearchInsert(nums, target);
             #endregion
 
--- Solution.cs
+++ Solution.cs
@@ -1,61 +1,123 @@
-﻿public class Solution
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
 {
 
-    #region https://leetcode.com/problems/search-insert-position/ search index position
+    /* #region https: //leetcode.com/problems/search-insert-position/ search index position
+     public int SearchInsert(int[] nums, int target)
+     {
+         int indexOfTarget = 0;
+         int lengthOfArray = nums.Length - 1;
+
+         if (lengthOfArray == 0)
+         {
+             if (nums[0] == target)
+             {
+                 return indexOfTarget;
+             }
+             else if (nums[0] > target)
+             {
+                 return 0;
+             }
+             else if (nums[0] < target)
+             {
+                 return indexOfTarget + 1;
+             }
+         }
+
+         for (int i = 0; i < nums.Length; i++)
+         {
+             if (nums[i] == target)
+             {
+                 indexOfTarget = i;
+                 return indexOfTarget;
+             }
+         }
+
+         if (indexOfTarget == 0)
+         {
+             for (int j = 0; j < nums.Length; j++)
+             {
+                 if (nums[j] < target && nums[j + 1] > target)
+                 {
+                     indexOfTarget = j;
+                     return indexOfTarget;
+                 }
+                 else if (nums[lengthOfArray] < target)
+                 {
+                     return lengthOfArray + 1;
+                 }
+             }
+         }
+         return 0;
+         #endregion
+     }*/
+
+    /*
+    Given a sorted array of distinct integers and a target value, return the index if the target is found.If not, return the index where it would be if it were inserted in order.
+
+    You must write an algorithm with O(log n) runtime complexity.
+
+    Example 1:
+
+    Input: nums = [1, 3, 5, 6], target = 5
+    Output: 2
+    Example 2:
+
+    Input: nums = [1, 3, 5, 6], target = 2
+    Output: 1
+    Example 3:
+
+    Input: nums = [1, 3, 5, 6], target = 7
+    Output: 4*/
+
     public int SearchInsert(int[] nums, int target)
     {
-        int indexOfTarget = 0;
-        int lengthOfArray = nums.Length - 1;
+        var Dict = new Dictionary<int, int>();
 
-        if (lengthOfArray == 0)
+        var index = 0;
+        var lower = 0;
+        var upper = 0;
+        foreach (var value in nums)
         {
-            if (nums[0] == target)
-            {
-                return indexOfTarget;
-            }
-            else if (nums[0] > target)
-            {
-                return 0;
-            }
-            else if (nums[0] < target)
-            {
-                return indexOfTarget + 1;
-            }
 
 
+            Dict.Add(value, index);
+            index++;
+        }
 
+
+        if (Dict.ContainsKey(target))
+        {
+            Console.WriteLine(Dict[target]);
+            Console.ReadLine();
+            return Dict[target];
         }
 
 
 
 
-
-        for (int i = 0; i < nums.Length; i++)
+        else if (!Dict.ContainsKey(target))
         {
-            if (nums[i] == target)
+            foreach (var value in Dict)
             {
-                indexOfTarget = i;
-                return indexOfTarget;
+
+                Console.WriteLine(Dict[value]);
+                Console.ReadLine();
             }
         }
 
-        if (indexOfTarget == 0)
-        {
-            for (int j = 0; j < nums.Length; j++)
-            {
-                if (nums[j] < target && nums[j + 1] > target)
-                {
-                    indexOfTarget = j;
-                    return indexOfTarget;
-                }
-                else if (nums[lengthOfArray] < target)
-                {
-                    return lengthOfArray + 1;
-                }
-            }
-        }
-        return 0;
-        #endregion
+        /*    foreach (var check in Dict)
+    {
+        Console.WriteLine(check);
+        Console.ReadLine();
+    }*/
+
+        return -1;
+
+
     }
 }
+
 
