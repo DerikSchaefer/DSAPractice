8f4de92256eae818474280529a48102458fdf48d
Derik <Derik.p.schaefer@gmail.com>
2023-02-02 17:49:36 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DSAPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+
+            #region https://leetcode.com/problems/search-insert-position/ search index position
+            /*      int[] nums = { 1, 3, 5, 6 };
+                  int target = 7;
+                  solution.SearchInsert(nums, target);*/
+            #endregion
+
+
+        }
+
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,36 @@
+﻿public class Solution
+{
+
+    #region https://leetcode.com/problems/search-insert-position/ search index position
+    /*    public int SearchInsert(int[] nums, int target)
+        {
+            int indexOfTarget = 0;
+            int lengthOfArray = nums.Length - 1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == target)
+                {
+                    indexOfTarget = i;
+                    return indexOfTarget;
+                }
+            }
+
+            if (indexOfTarget == 0)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[j] > target && nums[j + 1] > target)
+                    {
+                        indexOfTarget = j;
+                        return indexOfTarget;
+                    }
+                    else if (nums[lengthOfArray] < target)
+                    {
+                        return lengthOfArray + 1;
+                    }
+                }
+            }
+            return 0;*/
+    #endregion
+}
